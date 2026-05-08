138a55c2745cc946d62be9e4f79f46c4a7208e38
lucas-britto-dev <lucas.cruz@sptech.school>
2026-05-08 00:17:30 -0300

    feat: adiciona controller e route para usuário

---
+++ src/routes/usuarios.js
@@ -0,0 +1,15 @@
+let express = require("express");
+let router = express.Router();
+
+let usuarioController = require("../controllers/usuarioController")
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+router.post("/cadastrar", function (req, res) {
+    usuarioController.cadastrar(req, res);
+});
+
+router.post("/autenticar", function (req, res) {
+    usuarioController.autenticar(req, res);
+});
+
+module.exports = router;
