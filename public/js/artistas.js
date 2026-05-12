f3ed36da1befa55a7f8fecfc46f3d720395a7fab
lucas-britto-dev <lucas.cruz@sptech.school>
2026-05-11 22:55:21 -0300

    feat: tela de artista 90% finalizada, sem implementação de links

---
+++ public/js/artistas.js
@@ -0,0 +1,292 @@
+let artistas = [
+    {
+        "nome": "Tauz",
+        "foto": "./assets/imgs/artistas/tauz_artistas.jpg",
+        "link": "",
+        "inscritos": "9330000"
+    },
+    {
+        "nome": "7 Minutoz",
+        "foto": "./assets/imgs/artistas/7mz_artistas.jpg",
+        "link": "",
+        "inscritos": "14100000"
+    },
+    {
+        "nome": "VMZ",
+        "foto": "./assets/imgs/artistas/vmz_artistas.jpg",
+        "link": "",
+        "inscritos": "2040000"
+    },
+    {
+        "nome": "Daarui",
+        "foto": "./assets/imgs/artistas/daarui_artistas.jpg",
+        "link": "",
+        "inscritos": "255000"
+    },
+    {
+        "nome": "Felícia Rock",
+        "foto": "./assets/imgs/artistas/felicia_artistas.jpg",
+        "link": "",
+        "inscritos": "822000"
+    },
+    {
+        "nome": "oShaman",
+        "foto": "./assets/imgs/artistas/oshaman_artistas.jpg",
+        "link": "",
+        "inscritos": "85700"
+    },
+    {
+        "nome": "MHRAP",
+        "foto": "./assets/imgs/artistas/mhrap_artistas.jpg",
+        "link": "",
+        "inscritos": "4150000"
+    },
+    {
+        "nome": "Henrique Mendonça",
+        "foto": "./assets/imgs/artistas/henrique_artistas.jpg",
+        "link": "",
+        "inscritos": "554000"
+    },
+    {
+        "nome": "Shooter",
+        "foto": "./assets/imgs/artistas/shooter_artistas.jpg",
+        "link": "",
+        "inscritos": "146000"
+    },
+    {
+        "nome": "Shiny",
+        "foto": "./assets/imgs/artistas/shiny_artistas.jpg",
+        "link": "",
+        "inscritos": "228000"
+    },
+    {
+        "nome": "Anny",
+        "foto": "./assets/imgs/artistas/anny_artistas.jpg",
+        "link": "",
+        "inscritos": "221000"
+    },
+    {
+        "nome": "Enygma",
+        "foto": "./assets/imgs/artistas/enygma_artistas.jpg",
+        "link": "",
+        "inscritos": "1010000"
+    },
+    {
+        "nome": "M4rkim",
+        "foto": "./assets/imgs/artistas/m4_artistas.jpg",
+        "link": "",
+        "inscritos": "1160000"
+    },
+    {
+        "nome": "AniRap",
+        "foto": "./assets/imgs/artistas/anirap_artistas.jpg",
+        "link": "",
+        "inscritos": "1870000"
+    },
+    {
+        "nome": "Chrono",
+        "foto": "./assets/imgs/artistas/chrono_artistas.jpg",
+        "link": "",
+        "inscritos": "583000"
+    },
+    {
+        "nome": "Okabe",
+        "foto": "./assets/imgs/artistas/okabe_artistas.jpg",
+        "link": "",
+        "inscritos": "321000"
+    },
+    {
+        "nome": "Mistery",
+        "foto": "./assets/imgs/artistas/mistery_artistas.jpg",
+        "link": "",
+        "inscritos": "221000"
+    },
+    {
+        "nome": "Ishida",
+        "foto": "./assets/imgs/artistas/ishida_artistas.jpg",
+        "link": "",
+        "inscritos": "225000"
+    },
+    {
+        "nome": "Isis Vasconcellos",
+        "foto": "./assets/imgs/artistas/isis_artistas.jpg",
+        "link": "",
+        "inscritos": "1430000"
+    },
+    {
+        "nome": "Basara",
+        "foto": "./assets/imgs/artistas/basara_artistas.jpg",
+        "link": "",
+        "inscritos": "477000"
+    },
+    {
+        "nome": "Kaito",
+        "foto": "./assets/imgs/artistas/kaito_artistas.jpg",
+        "link": "",
+        "inscritos": "365000"
+    },
+    {
+        "nome": "Mands",
+        "foto": "./assets/imgs/artistas/mands_artistas.jpg",
+        "link": "",
+        "inscritos": "74500"
+    },
+    {
+        "nome": "Slow GM",
+        "foto": "./assets/imgs/artistas/slow_artistas.jpg",
+        "link": "",
+        "inscritos": "66200"
+    },
+    {
+        "nome": "OSteve",
+        "foto": "./assets/imgs/artistas/osteve_artistas.jpg",
+        "link": "",
+        "inscritos": "95400"
+    },
+    {
+        "nome": "Oshadow",
+        "foto": "./assets/imgs/artistas/oshadow_artistas.jpg",
+        "link": "",
+        "inscritos": "5430"
+    },
+    {
+        "nome": "ALBK",
+        "foto": "./assets/imgs/artistas/albk_artistas.jpg",
+        "link": "",
+        "inscritos": "129000"
+    },
+    {
+        "nome": "Taka",
+        "foto": "./assets/imgs/artistas/taka_artistas.jpg",
+        "link": "",
+        "inscritos": "168000"
+    },
+    {
+        "nome": "Gabriza",
+        "foto": "./assets/imgs/artistas/gabriza_artistas.jpg",
+        "link": "",
+        "inscritos": "224000"
+    },
+    {
+        "nome": "Novatroop",
+        "foto": "./assets/imgs/artistas/nova_artistas.jpg",
+        "link": "",
+        "inscritos": "295000"
+    },
+    {
+        "nome": "JRP",
+        "foto": "./assets/imgs/artistas/jrp_artistas.jpg",
+        "link": "",
+        "inscritos": "290000"
+    },
+    {
+        "nome": "AYAKASHI",
+        "foto": "./assets/imgs/artistas/ayakashi_artistas.jpg",
+        "link": "",
+        "inscritos": "53400"
+    },
+    {
+        "nome": "Neko",
+        "foto": "./assets/imgs/artistas/neko_artistas.jpg",
+        "link": "",
+        "inscritos": "126000"
+    },
+    {
+        "nome": "Nikmouu",
+        "foto": "./assets/imgs/artistas/nikmouu_artistas.jpg",
+        "link": "",
+        "inscritos": "76000"
+    },
+    {
+        "nome": "Blxck",
+        "foto": "./assets/imgs/artistas/blxck_artistas.jpg",
+        "link": "",
+        "inscritos": "212000"
+    },
+    {
+        "nome": "Ranori",
+        "foto": "./assets/imgs/artistas/ranori_artistas.jpg",
+        "link": "",
+        "inscritos": "54700"
+    },
+    {
+        "nome": "Igris",
+        "foto": "./assets/imgs/artistas/igris_artistas.jpg",
+        "link": "",
+        "inscritos": "71600"
+    },
+    {
+        "nome": "Chevz",
+        "foto": "./assets/imgs/artistas/chevz_artistas.jpg",
+        "link": "",
+        "inscritos": "18500"
+    },
+    {
+        "nome": "Everin",
+        "foto": "./assets/imgs/artistas/everin_artistas.jpg",
+        "link": "",
+        "inscritos": "185000"
+    },
+    {
+        "nome": "Raphyx",
+        "foto": "./assets/imgs/artistas/raphyx_artistas.jpg",
+        "link": "",
+        "inscritos": "121000"
+    },
+    {
+        "nome": "DK Zoom",
+        "foto": "./assets/imgs/artistas/dk_artistas.jpg",
+        "link": "",
+        "inscritos": "146000"
+    },
+    {
+        "nome": "ÉoDan",
+        "foto": "./assets/imgs/artistas/dan_artistas.jpg",
+        "link": "",
+        "inscritos": "287000"
+    },
+    {
+        "nome": "Neow",
+        "foto": "./assets/imgs/artistas/neow_artistas.jpg",
+        "link": "",
+        "inscritos": "32600"
+    }
+]
+
+let gridArtistas = document.getElementById('grid_artistas');
+let selectArtistas = document.getElementById('select');
+
+function iniciar() {
+    ordenarERenderizar('az');
+}
+
+function ordenarERenderizar(criterio) {
+    if(!criterio) {
+        criterio = selectArtistas.value;
+    }
+
+    let listaOrdenada = [...artistas];
+
+    if(criterio == 'az') {
+        listaOrdenada.sort((a, b) => a.nome.localeCompare(b.nome));
+    } else if (criterio === 'za') {
+        listaOrdenada.sort((a, b) => b.nome.localeCompare(a.nome));
+    } else if (criterio === 'mais_inscritos') {
+        listaOrdenada.sort((a, b) => +b.inscritos - +a.inscritos);
+    } else if (criterio === 'menos_inscritos') {
+        listaOrdenada.sort((a, b) => +a.inscritos - +b.inscritos);
+    }
+
+    renderizarCards(listaOrdenada);
+}
+
+function renderizarCards(lista) {
+    gridArtistas.innerHTML = lista.map(artistas => `
+        <div class="card_artista">
+            <img src="${artistas.foto}" alt="${artistas.nome}">
+            <span>${artistas.nome}</span>
+        </div>
+        `).join('');
+}
+
+iniciar();
