845c5edbe56fa18f730135493596aa129db3c960
lucas-britto-dev <lucas.cruz@sptech.school>
2026-05-13 22:52:11 -0300

    feat: adiciona links dos artistas

--- public/js/artistas.js
+++ public/js/artistas.js
@@ -2,253 +2,253 @@ let artistas = [
     {
         "nome": "Tauz",
         "foto": "./assets/imgs/artistas/tauz_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@Tauz",
         "inscritos": "9330000"
     },
     {
         "nome": "7 Minutoz",
         "foto": "./assets/imgs/artistas/7mz_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UCe6pKugTDyOPT1Md2DTybbQ",
         "inscritos": "14100000"
     },
     {
         "nome": "VMZ",
         "foto": "./assets/imgs/artistas/vmz_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UCgnKIwH7COU1ellkhepcvEg",
         "inscritos": "2040000"
     },
     {
         "nome": "Daarui",
         "foto": "./assets/imgs/artistas/daarui_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@DaaruiOficial",
         "inscritos": "255000"
     },
     {
         "nome": "Felícia Rock",
         "foto": "./assets/imgs/artistas/felicia_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UCo3Qfn9yCfDPYVqBawyg1lQ",
         "inscritos": "822000"
     },
     {
         "nome": "oShaman",
         "foto": "./assets/imgs/artistas/oshaman_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UCkjd1U496zW6V_fX9WBbZnQ",
         "inscritos": "85700"
     },
     {
         "nome": "MHRAP",
         "foto": "./assets/imgs/artistas/mhrap_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@MHRAPOFICIAL",
         "inscritos": "4150000"
     },
     {
         "nome": "Henrique Mendonça",
         "foto": "./assets/imgs/artistas/henrique_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@henriquemendonca",
         "inscritos": "554000"
     },
     {
         "nome": "Shooter",
         "foto": "./assets/imgs/artistas/shooter_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@Shooter_sz",
         "inscritos": "146000"
     },
     {
         "nome": "Shiny",
         "foto": "./assets/imgs/artistas/shiny_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@Shinysz",
         "inscritos": "228000"
     },
     {
         "nome": "Anny",
         "foto": "./assets/imgs/artistas/anny_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UCJSl43aVkxN_3j9MC2mABiQ",
         "inscritos": "221000"
     },
     {
         "nome": "Enygma",
         "foto": "./assets/imgs/artistas/enygma_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@Enygma_Music",
         "inscritos": "1010000"
     },
     {
         "nome": "M4rkim",
         "foto": "./assets/imgs/artistas/m4_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@M4rkim",
         "inscritos": "1160000"
     },
     {
         "nome": "AniRap",
         "foto": "./assets/imgs/artistas/anirap_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@AniRap",
         "inscritos": "1870000"
     },
     {
         "nome": "Chrono",
         "foto": "./assets/imgs/artistas/chrono_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@Chrono0",
         "inscritos": "583000"
     },
     {
         "nome": "Okabe",
         "foto": "./assets/imgs/artistas/okabe_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@OkabeOficial",
         "inscritos": "321000"
     },
     {
         "nome": "Mistery",
         "foto": "./assets/imgs/artistas/mistery_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UCnZfNrBtDmlwyLGx5ZGwrVA",
         "inscritos": "221000"
     },
     {
         "nome": "Ishida",
         "foto": "./assets/imgs/artistas/ishida_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@ishidaoficial",
         "inscritos": "225000"
     },
     {
         "nome": "Isis Vasconcellos",
         "foto": "./assets/imgs/artistas/isis_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@isisvasconcelllos",
         "inscritos": "1430000"
     },
     {
         "nome": "Basara",
         "foto": "./assets/imgs/artistas/basara_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@BasaraMusic",
         "inscritos": "477000"
     },
     {
         "nome": "Kaito",
         "foto": "./assets/imgs/artistas/kaito_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@KaitoOFC",
         "inscritos": "365000"
     },
     {
         "nome": "Mands",
         "foto": "./assets/imgs/artistas/mands_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@EahMands",
         "inscritos": "74500"
     },
     {
         "nome": "Slow GM",
         "foto": "./assets/imgs/artistas/slow_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UCo0hq1SW3fCxK61h-fqHmig",
         "inscritos": "66200"
     },
     {
         "nome": "OSteve",
         "foto": "./assets/imgs/artistas/osteve_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UCox78aIEke7X_CjUPaf6Q5Q",
         "inscritos": "95400"
     },
     {
         "nome": "Oshadow",
         "foto": "./assets/imgs/artistas/oshadow_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@oShadowOficial",
         "inscritos": "5430"
     },
     {
         "nome": "ALBK",
         "foto": "./assets/imgs/artistas/albk_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@ALBKMUSIC",
         "inscritos": "129000"
     },
     {
         "nome": "Taka",
         "foto": "./assets/imgs/artistas/taka_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@TakaB",
         "inscritos": "168000"
     },
     {
         "nome": "Gabriza",
         "foto": "./assets/imgs/artistas/gabriza_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UCefnj1ksvpLnpA28ZNMGgjQ",
         "inscritos": "224000"
     },
     {
         "nome": "Novatroop",
         "foto": "./assets/imgs/artistas/nova_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UCrSi0uwl1SPyd6-jrLkIVhg",
         "inscritos": "295000"
     },
     {
         "nome": "JRP",
         "foto": "./assets/imgs/artistas/jrp_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@jrp",
         "inscritos": "290000"
     },
     {
         "nome": "AYAKASHI",
         "foto": "./assets/imgs/artistas/ayakashi_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@abakashi",
         "inscritos": "53400"
     },
     {
         "nome": "Neko",
         "foto": "./assets/imgs/artistas/neko_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@n3koficial",
         "inscritos": "126000"
     },
     {
         "nome": "Nikmouu",
         "foto": "./assets/imgs/artistas/nikmouu_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UCkH2_fNQY6gkwfCbD3tKPuQ",
         "inscritos": "76000"
     },
     {
         "nome": "Blxck",
         "foto": "./assets/imgs/artistas/blxck_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UC2HoU9eqcVX7vCeavbLrqGw",
         "inscritos": "212000"
     },
     {
         "nome": "Ranori",
         "foto": "./assets/imgs/artistas/ranori_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UCKFS2H59meHpv7Z7xZ3cM9g",
         "inscritos": "54700"
     },
     {
         "nome": "Igris",
         "foto": "./assets/imgs/artistas/igris_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@Igrisofc",
         "inscritos": "71600"
     },
     {
         "nome": "Chevz",
         "foto": "./assets/imgs/artistas/chevz_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@Chevzz",
         "inscritos": "18500"
     },
     {
         "nome": "Everin",
         "foto": "./assets/imgs/artistas/everin_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@EverinMusic",
         "inscritos": "185000"
     },
     {
         "nome": "Raphyx",
         "foto": "./assets/imgs/artistas/raphyx_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UCW1rxe5XPOdDygYYqUgCtbQ",
         "inscritos": "121000"
     },
     {
         "nome": "DK Zoom",
         "foto": "./assets/imgs/artistas/dk_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UC2F0sKBd5TrAy-GEdjCnORg",
         "inscritos": "146000"
     },
     {
         "nome": "ÉoDan",
         "foto": "./assets/imgs/artistas/dan_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/@EoDanOficial",
         "inscritos": "287000"
     },
     {
         "nome": "Neow",
         "foto": "./assets/imgs/artistas/neow_artistas.jpg",
-        "link": "",
+        "link": "https://www.youtube.com/channel/UCauysPHzihO7mB8RHhpq3CQ",
         "inscritos": "32600"
     }
 ]
@@ -282,10 +282,12 @@ function ordenarERenderizar(criterio) {
 
 function renderizarCards(lista) {
     gridArtistas.innerHTML = lista.map(artistas => `
+        <a class="link_artista" href="${artistas.link}" target="_blank">
         <div class="card_artista">
             <img src="${artistas.foto}" alt="${artistas.nome}">
             <span>${artistas.nome}</span>
         </div>
+        </a>
         `).join('');
 }
 
