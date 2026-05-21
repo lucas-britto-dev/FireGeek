83a7fe477b70454b207cda6c8f23d6ad1d21c4b6
lucas-britto-dev <lucas.cruz@sptech.school>
2026-05-21 00:04:42 -0300

    feat: início da implementação das dashboards

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -13,7 +13,7 @@ function validarSessao() {
 
 function limparSessao() {
     sessionStorage.clear();
-    window.location = "../login.html";
+    window.location = "./index.html";
 }
 
 // carregamento (loading)
--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -3,7 +3,6 @@ let router = express.Router();
 
 let usuarioController = require("../controllers/usuarioController")
 
-//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
 router.post("/cadastrar", function (req, res) {
     usuarioController.cadastrar(req, res);
 });
